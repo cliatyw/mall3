b7dce7cb7b51d951bae54f4cdc44104273274779
Administrator <Administrator@MSDN-SPECIAL>
2018-04-05 10:10:04 +0900

    [김기성] maven + spring mvc + mybatis 수업내용

--- mall3/src/main/java/com/test/mall3/member/service/MemberDao.java
+++ mall3/src/main/java/com/test/mall3/member/service/MemberDao.java
@@ -5,8 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.test.mall3.IndexController;
@@ -14,37 +16,14 @@ import com.test.mall3.IndexController;
 @Repository
 public class MemberDao {
 	private static final Logger logger = LoggerFactory.getLogger(MemberDao.class);
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 	/*
 	 * Member를 매개변수로 받아 삽입하는 매서드
 	 */
 	public void insertMember(Member member) {
 		logger.info("insertMember");
-		PreparedStatement pstmt = null;
-		Connection conn = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			String jdbcDriver = "jdbc:mysql://localhost:3306/mall?" +
-					"useUnicode=true&characterEncoding=utf-8";
-			String dbUser = "root";
-			String dbPass = "java0000";
-			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
-			pstmt = conn.prepareStatement(
-					"INSERT INTO member VALUES (null, ?, ?)");
-			
-			pstmt.setString(1, member.getMemberId());
-			pstmt.setString(2, member.getMemberPw());
-			
-			pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if (conn != null) try { conn.close(); } catch(SQLException e) {}
-			if (pstmt != null) try { pstmt.close(); } catch(SQLException e) {}
-		}
+		
 	}
 }

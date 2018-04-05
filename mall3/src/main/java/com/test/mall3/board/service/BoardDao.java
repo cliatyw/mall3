b99fda396e3d0a21e1c80281ca55d6e1d2024bed
smart-202-16 <smart-202-16@smart-202-16-PC>
2018-04-05 10:19:46 +0900

    [김도희]mybatis 연동환경설정추가

--- mall3/src/main/java/com/test/mall3/board/service/BoardDao.java
+++ mall3/src/main/java/com/test/mall3/board/service/BoardDao.java
@@ -1,7 +1,9 @@
 package com.test.mall3.board.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.test.mall3.member.service.MemberService;
@@ -10,6 +12,9 @@ import com.test.mall3.member.service.MemberService;
 public class BoardDao {
 	private static final Logger logger = LoggerFactory.getLogger(MemberService.class);
 	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
 	public String insertBoard(Board board) {
 		logger.info(board.getBoardTitle()+"<--title");
 		logger.info(board.getBoardContent()+"<--content");

00387873d8ff88b31250ecbf70da3c03e4e3c2c3
smart-202-16 <smart-202-16@smart-202-16-PC>
2018-04-13 17:31:53 +0900

    [김도희]댓글db등록 까지

--- mall3/src/main/java/com/test/mall3/boardcomment/service/BoardComment.java
+++ mall3/src/main/java/com/test/mall3/boardcomment/service/BoardComment.java
@@ -3,13 +3,20 @@ package com.test.mall3.boardcomment.service;
 public class BoardComment {
 
 	private int commentNo;
+	private int boardNo;
 	private String commentContent;
-	
+	private String memberId;
 	public int getCommentNo() {
 		return commentNo;
 	}
 	public void setCommentNo(int commentNo) {
 		this.commentNo = commentNo;
+	}
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
 	}
 	public String getCommentContent() {
 		return commentContent;
@@ -17,8 +24,15 @@ public class BoardComment {
 	public void setCommentContent(String commentContent) {
 		this.commentContent = commentContent;
 	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
 	@Override
 	public String toString() {
-		return "BoardComment [commentNo=" + commentNo + ", commentContent=" + commentContent + "]";
-	}
+		return "BoardComment [commentNo=" + commentNo + ", boardNo=" + boardNo + ", commentContent=" + commentContent
+				+ ", memberId=" + memberId + "]";
+	}	
 }

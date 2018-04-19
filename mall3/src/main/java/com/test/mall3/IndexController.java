fc67591db86b1cc66acbb118431f4bd037a00ad7
Administrator <Administrator@MSDN-SPECIAL>
2018-04-19 16:24:48 +0900

    [김기성] 부트스트랩 적용 스타일 안

--- mall3/src/main/java/com/test/mall3/IndexController.java
+++ mall3/src/main/java/com/test/mall3/IndexController.java
@@ -1,18 +1,29 @@
 package com.test.mall3;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.test.mall3.category.service.Category;
+import com.test.mall3.category.service.CategoryService;
+
 @Controller
 public class IndexController {
+	@Autowired
+	private CategoryService categoryService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
 	@RequestMapping(value = {"/","/index"}, method = RequestMethod.GET)
-	public String home() {
+	public String home(Model model) {
+		List<Category> list = categoryService.getCategoryList();
+		model.addAttribute("list", list);
 		return "index";
 	}
 }
--- mall3/src/main/webapp/WEB-INF/views/index.jsp
+++ mall3/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <jsp:include page="header.jsp"></jsp:include>
-	<h1>index</h1>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-3 col-md-2 sidebar">
+				<ul class="nav nav-sidebar">
+					<c:forEach var="category" items="${list}">
+						<li><a href="${pageContext.request.contextPath}/getItemList?categoryNo=${category.categoryNo}&categoryName=${category.categoryName}">${category.categoryName}</a></li>
+					</c:forEach>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<h1>index</h1>
+	</div>
 </body>
 </html>
